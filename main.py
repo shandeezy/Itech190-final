f7b63df22a7e0ca69acdc6c0d12002f4a17c8f37
Shandell Martell <shmartell@stu.lltc.edu>
2024-12-12 08:24:08 -0800

    final

---
+++ main.py
@@ -0,0 +1,63 @@
+def print_list(lyst):
+    print(lyst)
+
+def for_loop(lyst):
+    for i in range(10)
+    print(lyst(len))
+
+def print_list_capitalize(lyst, upper):
+    print(lyst[upper{"pizza", "chicken", "apples", "bananas", "sugar", "strawberries", "blueberries", "hotdogs", "chips",}])
+
+def print_list_reverse(lyst):
+    print(lyst[-1::-1])
+
+def print_list_slice(lyst, start, stop):
+    print(lyst[start:stop])
+
+def main():
+    my_list = [
+        "pizza",
+        "chicken",
+        "apples",
+        "bananas",
+        "sugar",
+        "strawberries",
+        "blueberries",
+        "hotdogs",
+        "chips",
+        "cookies",
+    ]
+
+
+    
+    print_list(my_list)
+    print_list_reverse(my_list)
+    print_list_slice(my_list, 1, 3)
+    print_list_capitalize(my_list, upper)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
